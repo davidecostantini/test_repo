4f615e0f9eaff00125529a7863bcc2ec6831d53a
Davide 3500 <me@davidecostantini.it>
2016-01-08 23:39:36 +0000

    Small fix

--- configure.sh
+++ configure.sh
@@ -59,5 +59,6 @@ PGPASSWORD="$PS_PWD" psql -h localhost -Upostgres < initialize_db.sql
 ##################
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -P INPUT DROP
 /sbin/iptables-save

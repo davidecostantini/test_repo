7d16d9ab89b5102c287933dee82e9d9f85350296
Davide 3500 <me@davidecostantini.it>
2016-01-08 23:42:04 +0000

    Small fix

--- configure.sh
+++ configure.sh
@@ -37,7 +37,7 @@ apt-get install -y libapache2-mod-proxy-html libxml2-dev postgres-xc-client php5
 a2enmod ssl
 
 mkdir /etc/apache2/ssl
-cp apache2/apache.cert /etc/apache2/ssl/
+cp apache2/apache.crt /etc/apache2/ssl/
 cp apache2/apache.key /etc/apache2/ssl/
 
 ##################

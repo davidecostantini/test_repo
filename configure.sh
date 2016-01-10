a47aa0332db54d1ce879c575072a8336fca79e2e
root <root@ip-172-31-7-112.eu-west-1.compute.internal>
2016-01-10 12:51:02 +0000

    Added firewall rule for Postgres

--- configure.sh
+++ configure.sh
@@ -76,5 +76,6 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 8080 -s 127.0.0.1 -j ACCEPT
+iptables -A INPUT -p tcp --dport 5432 -s 127.0.0.1 -j ACCEPT
 iptables -P INPUT DROP
 /sbin/iptables-save
